757e35de40b8328790428f50bda909d21ea52886
Ragaviarumugam24 <your_github_email@example.com>
2025-12-03 00:32:41 +0530

    Add Dockerfile for Render deployment

--- obj/Debug/net10.0/EventManagement.AssemblyInfo.cs
+++ obj/Debug/net10.0/EventManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0855652150d5a5b9be217633f0133fb272f38789")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
